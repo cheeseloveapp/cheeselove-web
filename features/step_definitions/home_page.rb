de35e78521c383557d0bf5310037382d774c2d01
Chris Tucker Mear <chris@feedmechocolate.com>
2023-03-05 08:30:51 +0000

    wip: feature

---
+++ features/step_definitions/home_page.rb
@@ -0,0 +1,15 @@
+Given('I am on the home page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I visit the home page') do
+  visit(home_path)
+end
+
+When('I click on a cheese') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see a list of cheeses') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
