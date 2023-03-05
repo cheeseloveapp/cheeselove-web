f217113f3d542c9595e8ce2bd7e97e5d46fe99cd
Chris Tucker Mear <chris@feedmechocolate.com>
2023-03-05 09:08:21 +0000

    wip: home route

---
+++ spec/routing/home_routes_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe "routing to the home controller", type: :routing do
+  it "routes a named route" do
+    expect(get: home_path).
+      to route_to(controller: "home", action: "new")
+  end
+end
